7916b683de6712ccc1e5242949ca010ddad219c5
youngboxdill <serwer.paypal.132@gmail.com>
2025-09-24 12:05:58 +0200

    typy

--- skrypt.js
+++ skrypt.js
@@ -1,10 +1,20 @@
-let naszaZmienna = 4
+let a = 4 // Number
+console.log(typeof(a))
 
 
-console.log(naszaZmienna)
+let b = "JavaScript" // String
+console.log(typeof(b))
+let c = "JavaScript" // String
+console.log(typeof(c))
 
 
-naszaZmienna = 6
+let d = true // boolean (typu logicznego)
+console.log(typeof(d))
 
 
-console.log(naszaZmienna)
+let e = null // null (nic)
+console.log(typeof(e))
+
+
+let f
+console.log(typeof(f))
