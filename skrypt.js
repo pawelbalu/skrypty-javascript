880dcedd2155565701b2277f6c3c3924bf80a049
youngboxdill <serwer.paypal.132@gmail.com>
2025-09-24 12:15:58 +0200

    parzysta_nieparzysta

--- skrypt.js
+++ skrypt.js
@@ -1,9 +1,15 @@
 let x = 115
 
-if (x==15){
-    console.log('Wartość x = 15')
-}else if (x<15){
-    console.log('Wartość x jest mniejsza od 15')
-}else if (x>15){  // można napisac else tylko w tym przypadku
-    console.log('Wartość x jest większa od 15')
+// if (x==15){
+//     console.log('Wartość x = 15')
+// }else if (x<15){
+//     console.log('Wartość x jest mniejsza od 15')
+// }else if (x>15){  // można napisac else tylko w tym przypadku
+//     console.log('Wartość x jest większa od 15')
+// }
+
+if (x % 2 == 0){
+    console.log('Jest to liczba parzysta')
+}else{
+    console.log('Jest to liczba nieparzysta')
 }
