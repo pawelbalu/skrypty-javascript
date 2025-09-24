d83b9f4f4514ec9af1070b02d2dbe05af94e0de0
youngboxdill <serwer.paypal.132@gmail.com>
2025-09-24 12:34:26 +0200

    ifelse_case

--- skrypt.js
+++ skrypt.js
@@ -1,13 +1,15 @@
 // INSTRUKCJA WARUNKOWA SWITCH
 // break zatrzymuje działanie bo bez niego po case 5 wyświetla się w konsoli również default który nie powinien
 
-let x = 2
+let x = 3
 
 switch (x) {
     case 1:
     case 2:
     case 3:
-        console.log('x wynosi 1 lub 2 lub 3')
+        if (x==1) console.log('x wynosi 1')
+        else if (x==2) console.log('x wynosi 2')
+        else console.log ('x wynosi 3')
         break;
     case 4:
         console.log('x wynosi 4')
