ce42125d781d01860a33c6b8b76058eeac723bcf
youngboxdill <serwer.paypal.132@gmail.com>
2025-09-24 12:31:10 +0200

    case_break_lub

--- skrypt.js
+++ skrypt.js
@@ -1,14 +1,16 @@
 // INSTRUKCJA WARUNKOWA SWITCH
 // break zatrzymuje działanie bo bez niego po case 5 wyświetla się w konsoli również default który nie powinien
 
-let x = 5
+let x = 2
 
 switch (x) {
     case 1:
-        console.log('x wynosi 1')
-        break;
+    case 2:
     case 3:
-        console.log('x wynosi 3')
+        console.log('x wynosi 1 lub 2 lub 3')
+        break;
+    case 4:
+        console.log('x wynosi 4')
         break;
     case 5:
         console.log('x wynosi 5')
