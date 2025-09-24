e9e146ccb178dd53d46bc5d5e70448710f36068d
youngboxdill <serwer.paypal.132@gmail.com>
2025-09-24 12:07:20 +0200

    operatory_i_operatory_przypisania_komentarzowane

--- skrypt.js
+++ skrypt.js
@@ -1,20 +1,57 @@
-let a = 4 // Number
-console.log(typeof(a))
+// OPERATORY
 
 
-let b = "JavaScript" // String
-console.log(typeof(b))
-let c = "JavaScript" // String
-console.log(typeof(c))
+// let a = 15
+// let b = 4
 
 
-let d = true // boolean (typu logicznego)
-console.log(typeof(d))
+// console.log(a+b) dodawanie
+// console.log(a-b) odejmowanie
+// console.log(a*b) mnozenie
+// console.log(a/b) dzielenie
 
 
-let e = null // null (nic)
-console.log(typeof(e))
+// console.log(a%b) dzielenie z resztą
+// console.log(++a) inkrementacja
+// console.log(--b) dekrementacja
 
 
-let f
-console.log(typeof(f))
+// console.log(+a) dodatnia zmienna
+// console.log(-b) ujemna zmienna
+
+
+// console.log(2==2) równe liczby, jak są 3 = to bierze pod uwage typ
+// console.log(2!==2) czy nie jest równe
+// console.log(2>2) czy 2 jest większe od 2
+
+
+// OPERATORY PRZYPISANIA
+
+
+// let x = 2
+
+
+// console.log(x)
+
+
+// console.log(x+=2) // dodanie 2
+// console.log(x-=2) // odjęcie 2
+// console.log(x*=2) // mnożenie 2
+// console.log(x/=2) // dzielenie 2
+
+
+// let x = 1
+
+
+// console.log('Przesunięcie w lewo:'+ (x<<1))
+// console.log('Przesunięcie w prawo:'+ (x>>1))
+
+
+// console.log('Przesunięcie w prawo z wypełnieniem:'+ (x>>>1))
+
+
+// let x = -1
+// console.log('Przesunięcie w prawo:'+ (x>>1))
+
+
+// console.log('Przesunięcie w prawo z wypełnieniem:'+ (x>>>1))
