176409386f4ce5b2935f56bf5e8feff909cee89a
youngboxdill <serwer.paypal.132@gmail.com>
2025-09-24 12:14:03 +0200

    +-instrukcja

--- skrypt.js
+++ skrypt.js
@@ -1,5 +1,9 @@
-let x = 15
+let x = 115
 
 if (x==15){
     console.log('Wartość x = 15')
+}else if (x<15){
+    console.log('Wartość x jest mniejsza od 15')
+}else if (x>15){  // można napisac else tylko w tym przypadku
+    console.log('Wartość x jest większa od 15')
 }
