d299e9106b90a84be2f50e6dbc4b29dbd0ee80cf
youngboxdill <serwer.paypal.132@gmail.com>
2025-09-25 10:50:04 +0200

    obiekty_imie_this

--- skrypt.js
+++ skrypt.js
@@ -1,51 +1,13 @@
-const samochody = [
-    {
-    marka: 'Opel',
-    model: 'Astra',
-    przebieg: 120000,
-    cena: 10000,
-    ilosc: 3,
-    },
-    {
-        marka: 'Mazda',
-        model: '5',
-        przebieg: 140000,
-        cena: 12000,
-        ilosc: 4,
-    },
-    {
-        marka: 'Toyota',
-        model: 'Auris',
-        przebieg: 23000,
-        cena: 56000,
-        ilosc: 7,
-    },
-];
+// OBIEKTY
 
-// METODA MAP
-
-// const modyfikacjaCeny = samochody.map(function(e){
-//     return e.cena*1.2
-// });
-
-// console.log(samochody)
-// console.log(modyfikacjaCeny)
+var osoba = {
+    imie: 'Jan',
+    nazwisko: 'Kowalski',
+    wiek: 34,
+    show: function(){
+        console.log('Mam na imię '+this.imie+' a na nazwisko '+this.nazwisko+' mam '+this.wiek+' lat');
+    }
+}
 
 
-// METODA FILTER
-
-
-// const results = samochody.filter(function(e){
-//     return e.przebieg>130000
-// });
-
-// console.log(results)
-
-
-// METODA REDUCE
-
-const wartoscSamochodow = samochody.reduce(function(previousValue, currentValue){
-    return previousValue + currentValue.ilosc*currentValue.cena
-}, 0)
-
-console.log(wartoscSamochodow)
+osoba.show()
