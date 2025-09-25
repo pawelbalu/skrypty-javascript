4466a0196c6286a72c42a53ab740b27bcfe76413
youngboxdill <serwer.paypal.132@gmail.com>
2025-09-25 08:57:27 +0200

    pętla_while_do_while_obliczanie

--- skrypt.js
+++ skrypt.js
@@ -1,19 +1,55 @@
-// ISTRUKCJE ITERACYJNE (pętla) FOR
+// PĘTLA WHILE
 
-// 1. wyświetlanie i w konsoli po kolei
+// let i = 0
+// while (i<=10){
+//     console.log('kolejne przejście '+i)
 
-// for (let i = 0 ; i<=10 ; i++){
-//     console.log('kolejne przejście pętli '+i)
+//     i++
 // }
 
-// 2. parzyste
 
-// for (let i=2; i<=20; i++){
-//     if (i % 2 === 0) console.log(i)
+// OBLICZANIE LICZB
+
+// let a = parseInt(prompt('Podaj pierwszą liczbę')) // parse int konwertuje typ ze string na number
+// let b = parseInt(prompt('Podaj drugą liczbę'))
+// console.log(a+' '+typeof(a))
+
+
+// dalsza część vvv
+
+
+// let a = 0
+// let b = 0
+
+// let suma = 0
+
+// while (suma >= 0){
+//     let a = parseInt(prompt('Podaj pierwszą liczbę'))
+//     let b = parseInt(prompt('Podaj drugą liczbę'))
+//     suma = a+b
+//     console.log('suma podanych liczb wynosi '+suma)
 // }
 
-// 3. parzyste drugie rozwiązanie
+// kończy się gdy suma będzie na minusie
 
-for (let i=2; i<=20; i+=2){
-    console.log(i)
-}
+
+
+// PĘTLA DO WHILE
+
+// 1.
+// do {
+//     a = parseInt(prompt('Wprowadź pierwszą liczbę'))
+//     b = parseInt(prompt('Wprowadź drugą liczbę'))
+//     suma = a+b
+//     console.log('Suma podanych liczb wynosi: '+suma)
+
+// } while (suma >= 0)
+
+// 2. kończy gdy wynosi 0
+do {
+    a = parseInt(prompt('Wprowadź pierwszą liczbę'))
+    b = parseInt(prompt('Wprowadź drugą liczbę'))
+    suma = a+b
+    console.log('Suma podanych liczb wynosi: '+suma)
+
+} while (suma !== 0)
