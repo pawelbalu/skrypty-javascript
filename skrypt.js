b824ac77c0424503daf757c1d2de13475ec77606
youngboxdill <serwer.paypal.132@gmail.com>
2025-09-25 09:43:14 +0200

    foreach_samochody_tablice

--- skrypt.js
+++ skrypt.js
@@ -1,11 +1,34 @@
-const tablica = [1,2,3]
+const samochody = [
+    {
+    marka: 'Opel',
+    model: 'Astra',
+    przebieg: 120000,
+    cena: 10000,
+    ilosc: 3,
+    },
+    {
+        marka: 'Mazda',
+        model: '5',
+        przebieg: 140000,
+        cena: 12000,
+        ilosc: 4,
+    },
+    {
+        marka: 'Mazda',
+        model: '5',
+        przebieg: 230000,
+        cena: 12000,
+        ilosc: 4,
+    },
+];
 
-// tablica[0] = 8
+// console.log(samochody)
 
-tablica.unshift(8)
-tablica.push(4)
+// samochody.forEach(function(e) {console.log('!')})
 
-tablica.shift()
-tablica.pop()
+// console.log(samochody.length)
 
-console.log(tablica)
+samochody.forEach(function(e){
+    if (e.przebieg>130000) return console.log(e.marka)
+})
+
