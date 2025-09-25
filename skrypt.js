c9a198027e3bbd9d706ce00b93c6cd56214d71c9
youngboxdill <serwer.paypal.132@gmail.com>
2025-09-25 09:10:09 +0200

    tablice

--- skrypt.js
+++ skrypt.js
@@ -1,55 +1,11 @@
-// PĘTLA WHILE
+// TABLICE
 
-// let i = 0
-// while (i<=10){
-//     console.log('kolejne przejście '+i)
+/*
+const tablica = ['mleko', 'bułeczki', 'mąka', 'jajka', 12, true]
+console.log(tablica)
+*/
 
-//     i++
-// }
-
-
-// OBLICZANIE LICZB
-
-// let a = parseInt(prompt('Podaj pierwszą liczbę')) // parse int konwertuje typ ze string na number
-// let b = parseInt(prompt('Podaj drugą liczbę'))
-// console.log(a+' '+typeof(a))
-
-
-// dalsza część vvv
-
-
-// let a = 0
-// let b = 0
-
-// let suma = 0
-
-// while (suma >= 0){
-//     let a = parseInt(prompt('Podaj pierwszą liczbę'))
-//     let b = parseInt(prompt('Podaj drugą liczbę'))
-//     suma = a+b
-//     console.log('suma podanych liczb wynosi '+suma)
-// }
-
-// kończy się gdy suma będzie na minusie
-
-
-
-// PĘTLA DO WHILE
-
-// 1.
-// do {
-//     a = parseInt(prompt('Wprowadź pierwszą liczbę'))
-//     b = parseInt(prompt('Wprowadź drugą liczbę'))
-//     suma = a+b
-//     console.log('Suma podanych liczb wynosi: '+suma)
-
-// } while (suma >= 0)
-
-// 2. kończy gdy wynosi 0
-do {
-    a = parseInt(prompt('Wprowadź pierwszą liczbę'))
-    b = parseInt(prompt('Wprowadź drugą liczbę'))
-    suma = a+b
-    console.log('Suma podanych liczb wynosi: '+suma)
-
-} while (suma !== 0)
+/*
+const tablica = new Array('mleko', 'bułeczki', 'mąka', 'jajo', '12', 'true')
+console.log(tablica)
+*/
