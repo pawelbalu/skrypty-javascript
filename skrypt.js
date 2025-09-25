cbd3ac7ffddbb5482e04ec2a476cb1a04aebaea7
youngboxdill <serwer.paypal.132@gmail.com>
2025-09-25 08:32:04 +0200

    pętla for

--- skrypt.js
+++ skrypt.js
@@ -1,55 +1,19 @@
-// INSTRUKCJA WARUNKOWA SWITCH
-// break zatrzymuje działanie bo bez niego po case 5 wyświetla się w konsoli również default który nie powinien
+// ISTRUKCJE ITERACYJNE (pętla) FOR
 
-let x = 3
-let y = 5
-let z = 0
+// 1. wyświetlanie i w konsoli po kolei
 
-if (x!=y && x!=z && y!=z){ // sprawdzanie czy są różne
-    if(x>y && x>z){
-        console.log('Największa wartość to '+x)
-    }
-    if (y>x && y>z){
-        console.log('Największa wartość to '+y)
-    }
-    if (z>x && z>y){
-        console.log('Największa wartość to '+z)
-    }
+// for (let i = 0 ; i<=10 ; i++){
+//     console.log('kolejne przejście pętli '+i)
+// }
+
+// 2. parzyste
+
+// for (let i=2; i<=20; i++){
+//     if (i % 2 === 0) console.log(i)
+// }
+
+// 3. parzyste drugie rozwiązanie
+
+for (let i=2; i<=20; i+=2){
+    console.log(i)
 }
-
-
-
-
-// if (x>y){
-//     if (x>z){
-//         console.log('największa liczba to '+x)
-//     }else{
-//         console.log('największa liczba to '+z)
-//     }
-// }else if (y>x){
-//     if (y>z){
-//         console.log('największa liczba to '+y)
-//     }else{
-//         console.log('największa liczba to '+z)
-//     }
-// }
-
-
-
-// switch (x) {
-//     case 1:
-//     case 2:
-//     case 3:
-//         if (x==1) console.log('x wynosi 1')
-//         else if (x==2) console.log('x wynosi 2')
-//         else console.log ('x wynosi 3')
-//         break;
-//     case 4:
-//         console.log('x wynosi 4')
-//         break;
-//     case 5:
-//         console.log('x wynosi 5')
-//         break;
-//     default:
-//         console.log('żadna z powyższych opcji nie została wybrana')
-// }
