8d2fd88617e9c49efbe13843c2f7bc9ec33adfb9
youngboxdill <serwer.paypal.132@gmail.com>
2025-09-26 10:03:15 +0200

    zadanie praktyczne wykorzystujące funkcje cz 1.

--- skrypt.js
+++ skrypt.js
@@ -1,14 +1,48 @@
-const samochody = {
-    marka: 'Opel',
-    model: 'Astra',
-    wyswietl: function(){
-        console.log(this.model+' '+this.marka)
-    }
+let nr;
+let wybor;
+const pracownicy = new Array()
+
+function Osoba(imie,nazwisko){
+    this.imie = imie
+    this.nazwisko = nazwisko
 }
 
-// samochody.wyswietl()
+
+function tworzenie(imie,nazwisko){
+    osoba = new Osoba(imie,nazwisko)
+    pracownicy.push(osoba)
+}
+
+function usuwanie(nr){
+    delete pracownicy[nr]
+}
 
 
-let auto = samochody.wyswietl.bind(samochody);
+while(wybor!=0){
+    wybor = prompt('Podaj co chcesz zrobić. (1-tworzenie, 2-usuwanie, 3-wyświetlanie, 0-wyjście')
+    switch(wybor){
+    case '1':
+        let imie = prompt('podaj imię:')
+        let nazwisko = prompt('podaj nazwisko:')
+        tworzenie(imie,nazwisko)
+        break;
 
-auto()
+    case '2':
+        nr = prompt('podaj nr pracownika do usunięcia')
+        usuwanie(nr)
+        break;
+
+    case '3':
+        pracownicy.forEach((e)=>{
+            console.log(e)
+        })
+        break;
+
+    default:
+        document.write('Nie wybrano odpowiedniej opcji!')
+        break;
+}
+}
+
+
+console.log(pracownicy)
