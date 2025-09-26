39a29e883c9fb549cc198361be9e323db3b97f7a
youngboxdill <serwer.paypal.132@gmail.com>
2025-09-26 08:40:36 +0200

    Funkcje anonomiowe, strzałkowe jako alternatywa, operator spread - liczenie wielu liczb

--- skrypt.js
+++ skrypt.js
@@ -1,14 +1,32 @@
-// Składnia tworzenia funkcji
+// Funkcje anonomiowe, strzałkowe jako alternatywa, operator spread
 
-let a = 4
-let b = 8
+// let a = 3;
+// let b = 7;
+// let suma = 0;
 
-function dodawanie (x, y){
-    suma = x+y
-    return suma
-}
-
-dodawanie(a,b)
+// (function dodawanie (x,y){
+//     let suma = x+y
+//     return console.log(suma)
+// }(a,b));
 
 
-console.log(dodawanie(a,b))
+// let suma = (x,y) => console.log(x+y)
+
+// suma(a,b)
+
+
+
+// ((x,y) => console.log(x+y))(a,b)
+
+// co gdy duzo liczb?
+
+const liczby = [1,2,3,4,5,6]
+const liczby2 = [8,9,...liczby]
+
+console.log(liczby2)
+
+// function suma(a,b,c,d,e,f){
+//     return a+b+c+d+e+f
+// }
+
+// console.log(suma(...liczby))
