25a02ad853ae73aac3223670adc858cbf44e1b4f
youngboxdill <serwer.paypal.132@gmail.com>
2025-10-01 08:37:27 +0200

    Znajdowanie elementów przy pomocy JS - HTMLCollection, NodeList

--- skrypt.js
+++ skrypt.js
@@ -1,72 +1,16 @@
-let nr;
-let wybor;
-const pracownicy = new Array()
+// let kontener = document.getElementById('container')
 
-function Osoba(imie,nazwisko, numer){
-    this.imie = imie
-    this.nazwisko = nazwisko
-    this.numer = parseInt(numer)
-}
+// console.log(kontener)
+
+// let kontener1 = document.querySelector('#container')
+
+// console.log(kontener1)
 
 
-// function tworzenie(imie,nazwisko){
-//     let osoba = new Osoba(imie,nazwisko)
-//     pracownicy.push(osoba)
-// }
+let formularz = document.getElementsByTagName('form')
 
-// function usuwanie(nr){
-//     delete pracownicy[nr]
-// }
+console.log(formularz)
 
-const metody = {
-    tworzeniePracownika: function(imie,nazwisko,numer){
-        console.log(this)
-        let osoba = new Osoba(imie,nazwisko,numer)
-        pracownicy.push(osoba)
-    },
-    wyswietlPracownika: function(){
-        document.write('Imię: ',this.imie, ' nazwisko: ',this.nazwisko)
-    },
-    usuwaniePracownika: function(){
-        delete pracownicy[this.numer-1]
-    }
-}
+let formularz1 = document.querySelectorAll('form')
 
-
-while(wybor!=0){
-    wybor = prompt('Podaj co chcesz zrobić. (1-tworzenie, 2-usuwanie, 3-wyświetlanie, 0-wyjście')
-    switch(wybor){
-    case '1':
-        let imie = prompt('podaj imię:')
-        let nazwisko = prompt('podaj nazwisko:')
-        let numer = prompt('podaj numer pracownika')
-        let tworzenie = metody.tworzeniePracownika.bind(pracownicy)
-        tworzenie(imie,nazwisko,numer)
-        break;
-
-    case '2':
-        let nrP = prompt('podaj nr pracownika do usunięcia')
-        metody.usuwaniePracownika.apply(pracownicy[nrP])
-        wybor=0
-        break;
-
-    case '3':
-        pracownicy.forEach((e)=>{
-            console.log(e)
-        })
-        break;
-
-    case '4':
-        let nr = prompt('Podaj nr indeksu pracownika')
-        metody.wyswietlPracownika.call(pracownicy[nr])
-        wybor=0
-        break;
-
-    default:
-        document.write('Nie wybrano odpowiedniej opcji!')
-        break;
-}
-}
-
-
-console.log(pracownicy)
+console.log(formularz1)
